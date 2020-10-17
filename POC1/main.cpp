f20a4da3113f21a2d622d9da93b0a637f5a01121
Phileas Fogg <phileas.fogg@mail.com>
2020-10-17 19:38:40 +0200

    Start with unit testing

---
+++ POC1/main.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE SPEECHPOC1
+#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+
+
+
+BOOST_AUTO_TEST_SUITE( ts_speechPOC1)
+
+BOOST_AUTO_TEST_CASE( tc_todo )
+{
+  BOOST_TEST_REQUIRE( true == false );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
